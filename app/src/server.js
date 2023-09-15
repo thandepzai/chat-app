a707a0dabce180dd6cf9131d81a7f7d0fe26529e
thandepzai <91040602+thandepzai@users.noreply.github.com>
2023-09-15 10:34:17 +0700

    Update server.js

--- app/src/server.js
+++ app/src/server.js
@@ -84,7 +84,7 @@ io.on("connection", (socket) => {
 
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`app run on http://localhost:${port}`);
 });
